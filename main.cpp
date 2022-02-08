e53749b29fa2eacdebfb5cb95e10d7113127f04f
MightyHamburgerBun <71607087+MightyHamburgerBun@users.noreply.github.com>
2022-02-08 18:58:24 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
 
 int main() {
+    std::string tenants[10];
 
+    for (int i = 0; i < 10; ++i) {
+        std::cin >> tenants[i];
+    }
+
+    int apartment;
+    std::cout << "Apartment #";
+    std::cin >> apartment;
+    while (apartment < 0 || apartment >= 10) {
+        std::cout << "Input Error \n";
+        std::cout << "Apartment #";
+        std::cin >> apartment;
+    }
+    apartment -= 1;
+    std::cout << tenants[apartment] << std::endl;
 }
